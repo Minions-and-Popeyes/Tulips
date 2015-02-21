556e6d4cc8438b1c5de5c1cf10bdb32a8125a6ca
jiayu.sun <jiayu.sun@hulu.com>
2015-02-21 13:40:57 +0800

    bug fix

--- Models/Entities/lovebook.py
+++ Models/Entities/lovebook.py
@@ -10,13 +10,13 @@ class lovebook(object):
 	def save(self):
 		cur = cherrypy.request.cur
 		if self.id:
-			cur.execute("UPDATE love_book SET content = %s, time = %s, user = %s where id = %s",(self.content,self.time,self.user,self.id))
+			cur.execute("UPDATE lovebook SET content = %s, time = %s, user = %s where id = %s",(self.content,self.time,self.user,self.id))
 		else:
-			cur.execute("INSERT INTO love_book(content,time,user) VALUES(%s,%s,%s)",(self.content,self.time,self.user))
+			cur.execute("INSERT INTO lovebook(content,time,user) VALUES(%s,%s,%s)",(self.content,self.time,self.user))
 			self.id = cur.lastrowid
 	@staticmethod
 	def byid(cur,id):
-		cur.execute("SELECT id,content,time,user FROM love_book where id = %s",(id,))
+		cur.execute("SELECT id,content,time,user FROM lovebook where id = %s",(id,))
 		res = cur.fetchall()
 		if res:
 			return lovebook(*res[0])
